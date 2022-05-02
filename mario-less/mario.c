9f9149ff5e5e869e32e77c52305dfa007dd5a9c4
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-05-02 22:21:44 +0000

    Mon, May 2, 2022, 10:21 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,27 +4,3 @@
 int main(void)
 {
     int height;
-    int spaces;
-    int hash;
-
-do
-{
-    height = get_int("Height: ");
-}
-while (height > 1 || height < 8);
-
-for (int i = 0; i < height; i++)
-{
-    //For the loop to print out the spaces
-    for (spaces = (height - i); spaces >= 2; spaces--)
-    {
-        printf(" ");
-    }
-    // For loop to print out hashes
-    for (hash = 0; hash <= (i+1); hash++)
-    {
-        printf("#");
-    }
-    printf("\n");
-}
-}
