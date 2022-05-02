6b9900b0601a8884d85dfdf12d5be5e013b57915
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-05-02 23:04:41 +0000

    Mon, May 2, 2022, 11:04 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,26 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+int main(void) 
 {
-    int i=1;
-    int n=8;
-
-    do
-    {
-        printf("Height:");
-        n=GetInt();
-    } while (n<1 || n > 8);
-
-    for (i=1; i<n; i++)
-    {
-        printf(" ");
-        for (int x=0; x<i+2; x++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
-
-    return 0;
+    
 }
