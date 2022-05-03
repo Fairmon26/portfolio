97755ada1c7b0ec00811d25d2133d1c5dbf0b324
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-05-03 13:48:31 +0000

    Tue, May 3, 2022, 1:48 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -50,13 +50,13 @@ int get_cents(void)
 int calculate_quarters(int cents)
 {
     // TODO
-    int quarters
-    do
+    int quarters = 0;
+    while (cents >= 25)
     {
-        cents = get_int("Amount of change due");
+        cents = cents - 25;
+        quarters++;
     }
-    while (cents < 0);
-    return 0;
+    return quarters;
 }
 
 int calculate_dimes(int cents)
