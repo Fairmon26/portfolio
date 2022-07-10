2d7c7b2168d4caf8b1108fe0cb832cfd546f90af
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-07-10 12:11:59 +0000

    Sun, Jul 10, 2022, 12:11 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,9 +68,10 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (name == candidates[i].name)
+        if (strcmp(name, candidates[i].name) == 0)
         {
             candidates[i].votes++;
+            return true;
         }
     }
     return false;
@@ -79,6 +80,6 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    
     return;
 }
