6950c81259e9092d61f74c7e61322db93aa5ff7c
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-07-11 18:28:17 +0000

    Mon, Jul 11, 2022, 6:28 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -40,7 +40,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    RGBTRIPLE temp[height][width];
     {
         for(int j =0; j < width; j++)
         {
