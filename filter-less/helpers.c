d3ca24c909e4795bfff931c6053cff806c57e595
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-07-11 18:06:30 +0000

    Mon, Jul 11, 2022, 6:06 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,8 +9,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for(int j = 0; j < width; j++)
         {
             RGBTRIPLE pixel - image[i][j];
-
             int average = round(pixel.rgbtBlue + pixel.rgbtRed + pixel rgbtGreen)/3.0);
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
         }
     }
     return;
@@ -25,6 +25,14 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++)
+    {
+        for(int j =0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            float origiinalRed = pixel.rgbtRed;
+        }
+    }
     return;
 }
 
