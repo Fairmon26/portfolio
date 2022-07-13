d40647e21f80940b568073dfbab7ec740dbb1520
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-07-13 14:46:10 +0000

    Wed, Jul 13, 2022, 2:46 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -55,7 +55,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width / 2; i++)
+        for (int j = 0; j < width / 2; j++)
         {
             swap(&image[i][j], image[i][width - 1 - i]);
         }
