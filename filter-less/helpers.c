311df4e577ffa835c5e7c35f9697b2dc39fa79ae
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-07-13 16:05:08 +0000

    Wed, Jul 13, 2022, 4:05 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,9 +4,9 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i - 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j - 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             RGBTRIPLE pixel = image[i][j];
             int average = round(pixel.rgbtRed + pixel.rgbtGreen + pixelrgbtBlue) / 3.0)
