b859058db8146acc54f280a20250a4fb2817807b
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-07-13 17:50:55 +0000

    Wed, Jul 13, 2022, 5:50 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -74,12 +74,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE [height][width];
+
+    RGBTRIPLE temp[height][width];
     for(int i = 0; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
-            new_image[i][j] = get_blurred_pixel(i, j, height, width, image);
+            temp[i][j] = image[i][j];
         }
     }
 
