564e1f3537de269303072ae5869231b13a4174b0
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-07-13 18:25:36 +0000

    Wed, Jul 13, 2022, 6:25 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -28,8 +28,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for(int j = 0; j < width; j++)
         {
             float originalRed = image[i][j].rgbtRed;
-            float originalBlue = image[i][j].rgbtBlue;
             float originalGreen = image[i][j].rgbtGreen;
+            float originalBlue = image[i][j].rgbtBlue;
 
             int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
             int sepiaBlue = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
@@ -39,18 +39,18 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             {
                 sepiaRed = 255;
             }
-            if (sepiaBlue > 255)
-            {
-                sepiaBlue = 255;
-            }
             if (sepiaGreen > 255)
             {
                 sepiaGreen = 255;
             }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
 
             image[i][j].rgbtRed = sepiaRed;
-            image[i][j].rgbtBlue = sepiaBlue;
             image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
         }
     }
     return;
