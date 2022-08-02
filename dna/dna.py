b7748cf4562b513b7eaf4cbb04273be38314a01b
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-08-02 15:44:49 +0000

    Tue, Aug 2, 2022, 3:44 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -7,7 +7,7 @@ def main():
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
-        exit()
+        sys.exit()
 
     # TODO: Read database file into a variable
     with open (sys.argv[1]) as e:
