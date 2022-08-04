e637ed3f5369b177ba8cf2489a7dec104a168b4d
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-08-04 17:10:52 +0000

    Thu, Aug 4, 2022, 5:10 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,4 +2,4 @@ select title
 from people joinn stars on stars.person_id = people.id
 join movies on stars.movie_id = movies.id
 where name = "Johnny Depp"
-and movie_id in (select movie_id from people join stars on stars.person_id = people.id where name = "Helena Boham Carter")
+and movie_id in (select movie_id from people join stars on stars.person_id = people.id where name = "Helena Bonham Carter")
