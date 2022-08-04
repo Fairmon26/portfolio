de1cb9ff07aff6217786baa4f2bdba669566b599
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-08-04 17:23:45 +0000

    Thu, Aug 4, 2022, 5:23 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,5 @@
 select name
 from people
-join directors on directors.person_id = people_id
+join directors on directors.person_id = people.id
 join ratings on directors.movie_id = ratings.movie_id
 where ratings >= 9.0
