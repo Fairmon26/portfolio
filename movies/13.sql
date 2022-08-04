1cccad11dcd8aadfbca4904f97a7e8507806fc1f
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-08-04 17:31:32 +0000

    Thu, Aug 4, 2022, 5:31 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
 select distinct name
 from people join stars on stars.person_id = people.id
 where name != "Kevin Bacon"
-and movie_id in (select movie_id from people join stars on stars on stars.person_id = people.id where name = "Kevin Bacon" and birth = 1958)
+and movie_id in (select movie_id from people join stars on stars.person_id = people.id where name = "Kevin Bacon" and birth = 1958)
