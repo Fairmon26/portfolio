e268140922f8e9e595b5ad2f023f6f2aa8ba0bd9
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-08-04 17:45:22 +0000

    Thu, Aug 4, 2022, 5:45 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,3 +2,8 @@
 select description
 from crime_scene_reports
 where year = 2021 and month = 7 and day = 28 and street = "Humphrey Street"
+
+select transcript
+from interviews
+where year = 2021 and month 27 and day 28 and transcript like "%bakery%"
+
