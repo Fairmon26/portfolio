854c7054d6918ffc81cf525e98df6d283ea0fbc6
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-08-22 21:43:02 +0000

    Mon, Aug 22, 2022, 9:43 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -248,7 +248,7 @@ def sell():
     """Sell shares of stock"""
     if request.method == "GET":
         user_id = session["user_id"]
-        symbols_users = db.execute("SELECT symbole FROM transaction WHERE user_id = :id GROUP BY symbol HAVING SUM(shares) > 0" , id = user_id)
+        symbols_user = db.execute("SELECT symbole FROM transaction WHERE user_id = :id GROUP BY symbol HAVING SUM(shares) > 0" , id = user_id)
         return render_template("sell.html", symbols = [row["symbol"]for row in symbols_user])
 
     else:
