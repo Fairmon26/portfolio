660f96667da4398fcaf95bf1327cb4404aa584e6
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-08-22 19:09:26 +0000

    Mon, Aug 22, 2022, 7:09 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -80,7 +80,8 @@ def buy():
         uptd_cash = user_cash - transaction_value
 
         db.execute("UPDATE users SET cash = ? WHERE id = ?, user_id")
-        
+
+        date = datetime.datetime.now
 
 
 
