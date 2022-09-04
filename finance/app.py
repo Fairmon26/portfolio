3a90d7d72e105617af93cd6ad2e47437acb79e89
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-09-04 17:09:40 +0000

    Sun, Sep 4, 2022, 5:09 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -121,8 +121,6 @@ def add_cash():
         user_cash_db = db.execute("SELECT cash FROM users Where id = :id", id = user_id)
         user_cash = user_cash_db[0]["cash"]
 
-
-
         uptd_cash = user_cash + new_cash
 
         db.execute("UPDATE users SET cash = ? WHERE id = ?, uptd_cash, user_id")
