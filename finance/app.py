6c87b0e71696422058c5c26f314a3126b7be42ed
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-09-04 16:43:05 +0000

    Sun, Sep 4, 2022, 4:43 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -240,14 +240,6 @@ def sell():
         user_cash_db = db.execute("SELECT cash FROM users Where id = :id", id = user_id)
         user_cash = user_cash_db[0]["cash"]
 
-        user_shares = db.execute("SELECT shares FROM transaction WHERE user_id = :id AND symbol = :symbol GROUP BY symbol", id = user_id, symbol = symbol)
-        user_shares_real = user_shares[0]["shares"]
-
-        if shares > user_shares_real:
-            return apology("You Do Not Have This Amount Of Shares")
-
-
-
         uptd_cash = user_cash + transaction_value
 
         db.execute("UPDATE users SET cash = ? WHERE id = ?, uptd_cash, user_id")
