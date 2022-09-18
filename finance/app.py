0e3340e6aa63a6ff243e8604f2f3ce87aebbc16e
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-09-18 02:22:59 +0000

    Sun, Sep 18, 2022, 2:22 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -272,7 +272,7 @@ def sell():
 
         date = datetime.datetime.now()
 
-        db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", user_id, stock["symbol"], (-1) * shares, float(stock["price"]), date)
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", user_id, stock["symbol"], (-1) * shares, usd(float(stock["price"])), date)
 
         flash("Sold!")
 
