fe7701df8aded11285886bf46069aa1cdb607c65
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-12-03 03:42:46 +0000

    Sat, Dec 3, 2022, 3:42 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,42 +8,4 @@ int main(void)
     string text = get_string("Text: ");
     printf("%s\n", text);
 
-    int letters = 0;
-    for(int i = 0; i < strlen(text); i++)
-    {
-        if((text[i] >= 'a' && text[i] <= '2') || (text[i] >= 'A' && text[i] <= 'Z'))
-        letters++;
-    }
-
-    int words = 1;
-    for(int i = 0; i <strlen(text); i++)
-    {
-        if(text[i] == ' ')
-        words++;
-    }
-
-    int sentences = 0;
-    for(int i = 0; i < strlen(text); i++)
-    {
-        if(text[i] == '.' || text[i] == '!' || text[i] == '?')
-        sentences++;
-    }
-
-    float calculation = (0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 15.8;
-    int index = round(calculation);
-
-    if (index < 1)
-    {
-        printf("Before Grade 1\n");
-        return 0;
-    }
-    else if (index >= 16)
-    {
-        printf("Grade 16+\n");
-        return 0:
-    }
-    else
-    {
-        printf("Grade %i\n", index);
-    }
-}
+    
