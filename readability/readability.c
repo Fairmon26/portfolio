3ee75f639d7197d8729b08b4fac1861cd4d2608b
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-12-03 02:27:23 +0000

    Sat, Dec 3, 2022, 2:27 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,6 +11,9 @@ int main(void)
 
     for (int i = 0; i < strlen(text); i++)
     {
-        if (text[i] > 65 && text[i] < 90) || (text[i] > 97 && text[i] < 122)
+        if ((text[i] > 65 && text[i] < 90) || (text[i] > 97 && text[i] < 122))
+        {
+            letters++;
+        }
     }
 }
