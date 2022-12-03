3861964e16966b4d210252f4287ee74bdad59143
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-12-03 11:31:51 +0000

    Sat, Dec 3, 2022, 11:31 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,4 +7,14 @@ int main(void)
 {
     string text = get_string("Text: ");
     printf("%s\n", text);
+
+
+
+    int letters = 0;
+    for(int i = 0; i < strlen(text); i++)
+    {
+        if((text[i] >= 'a' && text[i] <= 'z' || text[i] >= 'A' && text[i] <= 'Z'))
+        letters++;
+    }
+    printf("%i letters", letters);
 }
