6603de550d10f5e0f8c3aa5e4dbe71fce73dd924
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-12-03 10:59:38 +0000

    Sat, Dec 3, 2022, 10:59 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,7 +16,7 @@ int main(void)
     }
 
     int words = 1;
-    for(int i = 0; i <strlen(text); i++)
+    for(int i = 0; i < strlen(text); i++)
     {
         if(text[i] == ' ')
         words++;
