3375f5212bed1696651144833d6ec138e77a0ec3
Fairmon26 <104722110+Fairmon26@users.noreply.github.com>
2022-12-03 02:30:16 +0000

    Sat, Dec 3, 2022, 2:30 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,10 +11,8 @@ int main(void)
 
     for (int i = 0; i < strlen(text); i++)
     {
-        if ((text[i] > 65 && text[i] < 90) || (text[i] > 97 && text[i] < 122))
-        {
-            if (isalpha(text[i]))
-        }
+        if (isalpha(text[i]))
+
         {
             letters++;
         }
